ccf0c4c13fa0e2af8b6feceec1d83b2f4724e09c
Tushar Tuteja <ttuteja@gmail.com>
2017-02-04 13:31:44 +0530

    basic authentication complete

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,53 @@ class HomeController < ApplicationController
 
 	def hello
 		@time = Time.now
-		time = Time.now + 2.days
+
+		if session[:user_id]
+			user = User.find(session[:user_id])
+		else
+			redirect_to '/signin'
+		end
+
 	end
 
+	def singup_get
+	end
 
-	
+	def signup
+		email = params["email"]
+		name = params["name"]
+		password = params["password"]
+
+		user = User.find_by_email(email)
+
+		unless user
+			user = User.create(email: email, password: password, name: name)
+			session[:user_id] = user.id
+			redirect_to '/'
+		end
+
+		return redirect_to '/signup'
+	end
+
+	def signin
+		email = params["email"]
+		password = params["password"]
+		user = User.find_by_email_and_password(email, password)
+		if user 
+			session[:user_id] = user.id
+			redirect_to '/'
+		else
+			redirect_to '/signin'
+		end
+
+	end
+
+	def signin_get
+	end
+
+	def logout
+		session[:user_id] = nil
+		return redirect_to '/'
+	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
   get 'launch/index'
 
   get '/' => 'home#hello'
+
+
+  get '/signin' => 'home#signin_get'
+  post '/signin' => 'home#signin'
+  get '/signup' => 'home#signup_get'
+  post '/signup' => 'home#signup'
+  get '/logout' => 'home#logout'
 end
